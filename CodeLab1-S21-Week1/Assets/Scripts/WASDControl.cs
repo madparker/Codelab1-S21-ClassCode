6ffce80f26f24e6a8d0bb69cc7f3d919c49e37a7
Matthew Parker <maddparker@gmail.com>
2021-02-01 19:52:50 -0600

    Adding Unity Project

---
+++ CodeLab1-S21-Week1/Assets/Scripts/WASDControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDControl : MonoBehaviour
+{
+
+    public float forceAmt = 10;
+
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * forceAmt);
+        }
+    }
+}
