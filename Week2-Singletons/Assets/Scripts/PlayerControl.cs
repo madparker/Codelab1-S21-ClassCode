278e977cbdae28472b80757d8b6dfc3c8754dd28
Matthew Parker <maddparker@gmail.com>
2021-02-08 18:24:34 -0600

    Added init scripts, sprite, and music

---
+++ Week2-Singletons/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float forceAmount = 5;
+    
+    Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2D.AddForce(Vector2.up * forceAmount);
+        }
+    }
+}
