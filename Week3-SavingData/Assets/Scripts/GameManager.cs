b288d8cc1a999438f8294b5848e7d98dedc5ead0
Matthew Parker <maddparker@gmail.com>
2021-02-18 16:58:17 -0600

    Added Week 3

---
+++ Week3-SavingData/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //static variable means the value is the same for all the objects of this class type and the class itself
+    public static GameManager instance; //this static var will hold the Singleton
+
+    public int score = 0;
+
+    int targetScore = 3;
+
+    int currentLevel = 0;
+
+    public TextMesh text;  //TextMesh Component to tell you the time and the score
+    
+    void Awake()
+    {
+        if (instance == null) //instance hasn't been set yet
+        {
+            DontDestroyOnLoad(gameObject);  //Dont Destroy this object when you load a new scene
+            instance = this;  //set instance to this object
+        }
+        else  //if the instance is already set to an object
+        {
+            Destroy(gameObject); //destroy this new object, so there is only ever one
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update the text with the score and level
+        text.text = "Level:" + currentLevel + " Score: " + score + " Target: " + targetScore;
+        
+        if (score == targetScore)  //if the current score == the targetScore
+        {
+            currentLevel++; //increase the level number
+            SceneManager.LoadScene(currentLevel); //go to the next level
+            targetScore += targetScore + targetScore/2; //update target score
+        }
+    }
+}
---
+++ Week3-SavingData/Assets/Scripts/Prize.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Prize : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+        transform.position = new Vector2( //teleport to a random location
+            Random.Range(-5,5),
+            Random.Range(-5,5));
+
+        GameManager.instance.score++; //increase the player's score using the Singleton!
+        
+        print("Score: " + GameManager.instance.score);
+    }
+}
