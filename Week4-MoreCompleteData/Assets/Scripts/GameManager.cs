720da8ac2c0db0adaa5841dc50938cd4034ac9f2
Matthew Parker <maddparker@gmail.com>
2021-02-22 18:40:11 -0600

    Init week 4 check in

---
+++ Week4-MoreCompleteData/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float gameTime = 10;
+
+    public Text timerText;
+    
+    float timer = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        timerText.text = "Time: " + (int)(gameTime - timer + 1);
+        
+        Debug.Log("Time: " + timer);
+
+        if (gameTime < timer) //Time is up
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ Week4-MoreCompleteData/Assets/Scripts/WhackADot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhackADot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked on Dot!!!");
+
+        transform.position = 
+            new Vector2(
+                Random.Range(-6f, 6f),
+                Random.Range(-4f, 4f));
+    }
+}
