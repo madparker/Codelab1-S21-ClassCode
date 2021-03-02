226ac3be1c51206eabbb72b7f0bffef84967c5d0
Matthew Parker <maddparker@gmail.com>
2021-03-01 20:37:52 -0600

    Added Goal Script

---
+++ Week5-ASCIILevelLoader/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameManager.instance.GetComponent<ASCIILevelLoader>().CurrentLevel++;
+    }
+}
