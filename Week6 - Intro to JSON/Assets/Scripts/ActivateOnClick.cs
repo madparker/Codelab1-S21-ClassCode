b64eee2cf85f8147981e81911fc48a84689a1b51
Matthew Parker <maddparker@gmail.com>
2021-03-08 18:45:51 -0600

    Init Week 6

---
+++ Week6 - Intro to JSON/Assets/Scripts/ActivateOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateOnClick : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        rigidbody.isKinematic = false;
+    }
+}
---
+++ Week6 - Intro to JSON/Assets/Scripts/MovableObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDrag()
+    {
+
+
+        transform.position = GetMouseWorldPosition();
+    }
+
+    Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+
+        mousePosition.z = 
+            Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
