595f3cdbb083b81587f47ade8c09fee33e8edae2
Matthew Parker <maddparker@gmail.com>
2021-03-08 19:47:13 -0600

    End of class commit w/o comments

--- Week6 - Intro to JSON/Assets/Scripts/MovableObject.cs
+++ Week6 - Intro to JSON/Assets/Scripts/MovableObject.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MovableObject : MonoBehaviour
 {
+
+    string filePath;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        filePath = Application.dataPath + "/" + name + ".json";
+
+        if (File.Exists(filePath))
+        {
+            string jsonStr = File.ReadAllText(filePath);
+
+            Vector3 savePosition = JsonUtility.FromJson<Vector3>(jsonStr);
+
+            transform.position = savePosition;
+        }
     }
 
     // Update is called once per frame
@@ -18,9 +31,15 @@ public class MovableObject : MonoBehaviour
 
     void OnMouseDrag()
     {
-
-
         transform.position = GetMouseWorldPosition();
+    }
+
+    void OnApplicationQuit()
+    {
+        string jsonPosition = 
+            JsonUtility.ToJson(transform.position, true);
+        Debug.Log(jsonPosition);
+        File.WriteAllText(filePath, jsonPosition);
     }
 
     Vector3 GetMouseWorldPosition()
