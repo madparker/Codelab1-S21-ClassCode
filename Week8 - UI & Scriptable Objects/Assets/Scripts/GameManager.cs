340a22d446d7497e236ae7169a02252125c601fb
Matthew Parker <maddparker@gmail.com>
2021-03-22 18:38:25 -0500

    Init Commit for Week 8

---
+++ Week8 - UI & Scriptable Objects/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public LocationScriptableObject currentLocation;
+
+    public Text locationNameText;
+    public Text locationDescription;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateLocation();
+    }
+
+    public void MoveDirection(int dir)
+    {
+        switch (dir)
+        {
+            case 0:
+                currentLocation = currentLocation.locationNorth;
+                break;
+            case 1:
+                currentLocation = currentLocation.locationSouth;
+                break;
+        }
+        
+        UpdateLocation();
+    }
+
+    void UpdateLocation()
+    {
+        locationNameText.text = currentLocation.locationName;
+        locationDescription.text = currentLocation.description;
+    }
+
+}
---
+++ Week8 - UI & Scriptable Objects/Assets/Scripts/ScriptableObjects/LocationScriptableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Location", 
+    menuName = "ScriptableObjects/Location", order = 0)]
+
+public class LocationScriptableObject : ScriptableObject
+{
+    public string locationName = "New Place";
+    public string description = "Default Description";
+
+    public LocationScriptableObject locationNorth;
+    public LocationScriptableObject locationSouth;
+    public LocationScriptableObject locationEast;
+    public LocationScriptableObject locationWest;
+}
