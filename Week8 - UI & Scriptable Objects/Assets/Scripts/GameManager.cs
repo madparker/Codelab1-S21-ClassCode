fff30a0fa31e6349c700a51e23d9f165d24c14fb
Matthew Parker <maddparker@gmail.com>
2021-03-24 09:00:37 -0500

    No Comments

--- Week8 - UI & Scriptable Objects/Assets/Scripts/GameManager.cs
+++ Week8 - UI & Scriptable Objects/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,10 +10,18 @@ public class GameManager : MonoBehaviour
 
     public Text locationNameText;
     public Text locationDescription;
+
+    public GameObject buttonNorth;
+    public GameObject buttonSouth;
+    public GameObject buttonWest;
+    public GameObject buttonEast;
     
     // Start is called before the first frame update
     void Start()
     {
+        TextAsset textAsset = Resources.Load<TextAsset>("LoadFile");
+        Debug.Log("In my file: " + textAsset.text);
+        
         UpdateLocation();
     }
 
@@ -26,6 +35,14 @@ public class GameManager : MonoBehaviour
             case 1:
                 currentLocation = currentLocation.locationSouth;
                 break;
+            case 2:
+                currentLocation = currentLocation.locationEast;
+                break;
+            case 3:
+                currentLocation = currentLocation.locationWest;
+                break;
+            default:
+                break;
         }
         
         UpdateLocation();
@@ -35,6 +52,46 @@ public class GameManager : MonoBehaviour
     {
         locationNameText.text = currentLocation.locationName;
         locationDescription.text = currentLocation.description;
+
+        if (currentLocation.locationEast == null)
+        {
+            buttonEast.SetActive(false);
+        }
+        else
+        {
+            currentLocation.locationEast.locationWest = currentLocation;
+            buttonEast.SetActive(true);
+        }
+
+        if (currentLocation.locationNorth == null)
+        {
+            buttonNorth.SetActive(false);
+        }
+        else
+        {
+            currentLocation.locationNorth.locationSouth = currentLocation;
+            buttonNorth.SetActive(true);
+        }
+
+        if (currentLocation.locationSouth == null)
+        {
+            buttonSouth.SetActive(false);
+        }
+        else
+        {
+            currentLocation.locationSouth.locationNorth = currentLocation;
+            buttonSouth.SetActive(true);
+        }
+
+        if (currentLocation.locationWest == null)
+        {
+            buttonWest.SetActive(false);
+        }
+        else
+        {
+            currentLocation.locationWest.locationEast = currentLocation;
+            buttonWest.SetActive(true);
+        }
     }
 
 }
