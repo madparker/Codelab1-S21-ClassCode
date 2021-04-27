797676823e340d08c0a1fc67e6a04046bb11b813
Matthew Parker <maddparker@gmail.com>
2021-04-26 19:50:29 -0500

    Added Triple Attack

--- Week11-CodeLab1-ObjectOrientedProgramming/Assets/Scripts/Ship/Attacks/BaseAttack.cs
+++ Week11-CodeLab1-ObjectOrientedProgramming/Assets/Scripts/Ship/Attacks/BaseAttack.cs
@@ -8,15 +8,18 @@ public class BaseAttack : MonoBehaviour
     
     public virtual void Attack()
     {
-        Debug.Log("Attack!!!");
-
-        GameObject bullet = Instantiate<GameObject>(bulletPrefab);
-
-        bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000);
-        
         Vector2 currentPos = transform.position;
         currentPos.y += 1f;
+        
+        SpawnBullet(currentPos);
+    }
 
+    public virtual void SpawnBullet(Vector2 currentPos)
+    {
+        GameObject bullet = Instantiate<GameObject>(bulletPrefab);
+
+        bullet.GetComponent<Rigidbody2D>().gravityScale = -1;
+        
         bullet.transform.position = currentPos;
     }
 }
---
+++ Week11-CodeLab1-ObjectOrientedProgramming/Assets/Scripts/Ship/Attacks/TripleAttack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TripleAttack : BaseAttack
+{
+    public override void SpawnBullet(Vector2 currentPos)
+    {
+        base.SpawnBullet(currentPos);
+        currentPos.x += 0.5f;
+        base.SpawnBullet(currentPos);
+        currentPos.x -= 1f;
+        base.SpawnBullet(currentPos);
+    }
+}
